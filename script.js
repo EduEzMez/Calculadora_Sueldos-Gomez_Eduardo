a295ec297526e3647d3cc69cd326332482d8eb7c
Calidad1 <calidaddosantos@gmail.com>
2024-06-19 15:51:53 -0300

    Ajuste de codigo js

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-let categoria = document.getElementById('categoria').value;
-let horasTrabajadas = document.getElementById('horas_trabajadas').value;
-let antiguedad = document.getElementById('antiguedad').value;
-let aportes = document.getElementById('aportes').value;
-let btn = document.getElementById('btn');
+let miFormulario = document.getElementById('formulario');
+miFormulario.addEventListener("submit", validacionFormulario);
 
-let sumar = 
+let resultado = document.getElementById('resultado')
 
-console.log(categoria)
+function validacionFormulario(evento){
+    evento.preventDefault();
+    let formulario = evento.target
+let datoA = formulario.children[1].value;
+let datoB = formulario.children[3].value;
+let datoC = formulario.children[5].value;
+let datoD = formulario.children[7].value;
+
+let mostar = Number(Math.trunc((datoA * datoB) + (datoA / datoB * datoC) - datoD));
+resultado.innerText = 'Sueldo a cobrar es de: $' + mostar + ' .-';
+
+}
+
+

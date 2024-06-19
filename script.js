aa4124f31e474a6f5b3c5f80bb4a980581afc2b7
32m32 <edu.ezequielgomez@mail.com>
2024-06-18 21:51:19 -0300

    lets creados en js

---
+++ script.js
@@ -0,0 +1,9 @@
+let categoria = document.getElementById('categoria').value;
+let horasTrabajadas = document.getElementById('horas_trabajadas').value;
+let antiguedad = document.getElementById('antiguedad').value;
+let aportes = document.getElementById('aportes').value;
+let btn = document.getElementById('btn');
+
+let sumar = 
+
+console.log(categoria)

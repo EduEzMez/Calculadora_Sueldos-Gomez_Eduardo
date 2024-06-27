bb7e87f04c98c0c22e118b71e1be8e50880303fd
ezmezlab <edu.ezequielgomez@gmail.com>
2024-06-26 21:37:03 -0300

    Ultimo Ajuste

--- js/script.js
+++ js/script.js
@@ -16,22 +16,22 @@ function miFormulario(evento){
     evento.preventDefault();
     let formulario = evento.target;
 
-    let dato0 = (formulario.children[1].value);
-    let datoA = Number(formulario.children[3].value);
-    let datoB = Number(formulario.children[5].value);
-    let datoC = Number(formulario.children[7].value);
-    let datoD = Number(formulario.children[9].value);
-    let datoH = Number(formulario.children[11].value);
+    let nombre = (formulario.children[1].value);
+    let categoria = Number(formulario.children[3].value);
+    let horas = Number(formulario.children[5].value);
+    let antiguedad = Number(formulario.children[7].value);
+    let adicionales = Number(formulario.children[9].value);
+    let aportes = Number(formulario.children[11].value);
 
-    let operacion = Math.trunc((datoA * datoB) + (datoA / datoB * datoC) + datoD - datoH);
+    let operacion = Math.trunc((categoria * horas) + (categoria / horas * antiguedad) + adicionales - aportes);
     let resultado = document.getElementById('resultado');
     resultado.innerText = 'El sueldo a cobrar es de: $' + operacion + '.-'
-    let nombre = document.getElementById('nombre').value;
+    nombre = document.getElementById('nombre').value;
     localStorage.setItem('nombre', nombre)
     let tomarDato = localStorage.getItem('nombre',nombre)
     tituloSaludo.innerText= 'Hola '+ tomarDato + ", bienvenido al simulador de sueldos."
 
-    let listDatos = {dato0,datoA,datoB,datoC,datoD,datoH}
+    let listDatos = {nombre,categoria,horas,antiguedad,adicionales,aportes}
     let objetoJson = JSON.stringify(listDatos)
     console.log(objetoJson)
 

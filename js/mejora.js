5af84374db5e51e3d4e8bb3c3ebafd45753ac132
Calidad1 <calidaddosantos@gmail.com>
2024-07-08 15:09:50 -0300

    Agregadas las funciones de grabado, lectura y eliminacion de localStorage

--- js/mejora.js
+++ js/mejora.js
@@ -1,21 +1,11 @@
-
 let simulador = document.getElementById('formulario');
 
-let tomarDato = localStorage.getItem('nombre',nombre)
-saludo.innerHTML = tomarDato;
-
-let tituloSaludo = document.getElementById('saludo');
-if (tomarDato == null){
-    tituloSaludo.innerText= 'Hola, bienvenido al simulador de sueldos. Complete los campos'
-} else{
-    tituloSaludo.innerText= 'Hola '+ tomarDato + ", bienvenido al simulador de sueldos."
-}
+let datosGuardados = JSON.parse(localStorage.getItem('datos')) || [];
 
 simulador.addEventListener('submit', miFormulario);
 function miFormulario(evento){
 
     evento.preventDefault();
-
     let formulario = evento.target;
 
     let nombre = (formulario.children[1].value);
@@ -29,28 +19,18 @@ function miFormulario(evento){
     let resultado = document.getElementById('resultado');
 
     let listDatos = {nombre,categoria,horas,antiguedad,adicionales,aportes,operacion}
-    let objetoJson = JSON.stringify(listDatos)
-    let objetoObjeto = JSON.parse(objetoJson)
-    //let getNombre = objetoObjeto.nombre
-    //let getOperacion = objetoObjeto.operacion
-
-
-    //ver este codigo si no se borra
-    let tomarNombre = localStorage.getItem('nombre',nombre)
-    let tomarOperacion = localStorage.getItem('operacion', operacion)
-    if (tomarNombre && tomarOperacion){
-        resultado.innerHTML += 'El sueldo a cobrar de '+ tomarNombre +' es de '+ tomarOperacion +'.-'
-    }
-
-    //resultado.innerHTML += `<p>El sueldo a cobrar de ${getNombre} es de $${getOperacion}.-</p>`
-    console.log(objetoJson)
-    console.log(objetoObjeto)
     
+    datosGuardados.push(listDatos)
+
+    localStorage.setItem('datos', JSON.stringify(datosGuardados))
+
     nombre = document.getElementById('nombre').value;
     localStorage.setItem('nombre', nombre)
     localStorage.setItem('operacion', operacion)
-    let tomarDato = localStorage.getItem('nombre',nombre)
-    tituloSaludo.innerText= 'Hola '+ tomarDato + ", bienvenido al simulador de sueldos."
+    tomarNombre = localStorage.getItem('nombre',nombre)
+    tomarOperacion = localStorage.getItem('operacion', operacion)
+    resultado.innerHTML += 'El sueldo a cobrar de '+ tomarNombre +' es de $'+ tomarOperacion +'.-'
+
 
     formulario.children[1].value = "";
     formulario.children[3].value = "";
@@ -59,7 +39,6 @@ function miFormulario(evento){
     formulario.children[9].value = "";
     formulario.children[11].value = "";
 
-    
     Toastify({
         text: "Calculo Realizado",
         className: "info",
